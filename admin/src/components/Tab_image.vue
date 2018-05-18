2d4d23f3a8934550a0cec4cdb7b6cb48f7e1e1b6
tecWang <tecwang@139.com>
2018-05-18 10:11:52 +0800

    整理目录

--- admin/src/components/Tab_image.vue
+++ admin/src/components/Tab_image.vue
@@ -25,6 +25,35 @@
                 </div>
             </div>
         </div>
+
+        <!-- 模态框 -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">修改图片链接</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="recipient-name" class="col-form-label">填入新的链接:</label>
+                                <input type="text" class="form-control" id="recipient-name" v-model="changedLink">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <div id="stateBtn" class="alert alert-success invisible" role="alert" style="padding: .375rem .75rem; margin-bottom: 0; width: 100%; ">
+                            修改成功
+                        </div>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
+                        <button type="button" class="btn btn-primary" @click="sendCardMsg">确认修改</button>
+                    </div>
+                </div>
+            </div>
+        </div> 
     </div>
 </template>
 
@@ -37,20 +66,44 @@ export default {
   name: 'Tab_image',
   data: function(){
         return {
-            cards: []
+            cards: [],
+            changedLink: "",
         }
   },
   mounted: function(){
     
         this.$http.get('http://192.168.1.113:8080/yong/UploadServlet').then(response => {
              this.cards = response.data.data;
-             console.log(this.cards);
-            // get body data
-            // this.someData = response.body;
-
         }, response => {
             console.log("error");
         });
+  },
+  methods: {
+        sendCardMsg: function(){
+            this.$http.post('http://192.168.1.113:8080/yong/ImageChangeServlet', 
+            {
+                id: sessionStorage.getItem('image_id'),
+                newLink: this.changedLink
+            }, {emulateJSON: true})
+            .then(response=>{
+                let data = response.data;
+                if(data.status == 1){
+                    // console.log(this);
+                    $('#stateBtn').addClass('visible');
+                    $('#stateBtn').removeClass('invisible');
+                    setTimeout(() => {
+                        $('#exampleModal').modal('toggle');
+                    }, 500);
+                }
+            }, response=> {
+                $('#stateBtn').addClass('visible');
+                $('#stateBtn').removeClass('invisible');
+                $('#stateBtn').addClass('alert-danger');
+                $('#stateBtn').removeClass('alert-success');
+                $('#stateBtn').html("修改失败！");
+                console.log("Change image data error");
+            });
+        }
   },
   components: {
     "tec-split": Split,
--- admin/src/components/card.vue
+++ admin/src/components/card.vue
@@ -5,39 +5,10 @@
             <h5 class="card-title" style="margin-bottom: 0;">{{cardData.image_Name}}</h5>
         </div>
         <ul class="list-group list-group-flush">
-            <li class="list-group-item" data-toggle="modal" data-target="#exampleModal">{{cardData.image_Target_Link}}</li>
-            <!-- <input type="text" class="list-group-item" :value="cardData.image_Target_Link"> -->
+            <li class="list-group-item" @click="toggleModal(this)">{{cardData.image_Target_Link}}</li>
+            <li class="list-group-item" v-show="cardData.image_Is_Used == 1">已启用</li>
+            <li class="list-group-item" v-show="cardData.image_Is_Used != 1">未启用</li>
         </ul>
-
-
-        <!-- 模态框 -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">修改图片链接</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form>
-                <div class="form-group">
-                    <label for="recipient-name" class="col-form-label">填入新的链接:</label>
-                    <input type="text" class="form-control" id="recipient-name" v-model="newLink">
-                </div>
-                </form>
-            </div>
-            <div class="modal-footer">
-                <div id="stateBtn" class="alert alert-success invisible" role="alert" style="padding: .375rem .75rem; margin-bottom: 0; width: 100%; ">
-                    修改成功
-                </div>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">关闭</button>
-                <button type="button" class="btn btn-primary" @click="sendCardMsg">确认修改</button>
-            </div>
-            </div>
-        </div>
-        </div> 
     </div>
 </template>
 
@@ -49,31 +20,14 @@ export default {
     },
     data(){
         return {
-            cardData: this.data,
-            newLink: ''
+            cardData: this.data
         }
     },
     methods: {
-        sendCardMsg: function(){
-            this.$http.post('http://192.168.1.113:8080/yong/ImageChangeServlet', {newLink: this.newLink}, {emulateJSON: true}).then(response=>{
-                // console.log(response.data);
-                let data = response.data;
-                if(data.status == 1){
-                    $('#stateBtn').addClass('visible');
-                    $('#stateBtn').removeClass('invisible');
-                    setTimeout(() => {
-                        $('#exampleModal').modal('toggle');
-                        this.cardData.image_Target_Link = this.newLink;
-                    }, 500);
-                }
-            }, response=> {
-                $('#stateBtn').addClass('visible');
-                $('#stateBtn').removeClass('invisible');
-                $('#stateBtn').addClass('alert-danger');
-                $('#stateBtn').removeClass('alert-success');
-                $('#stateBtn').html("修改失败！");
-                console.log("Change image data error");
-            });
+        toggleModal(dom){
+            $('#exampleModal').modal('toggle');
+            let id = this.$data.cardData.image_ID;
+            sessionStorage.setItem("image_id", id);
         }
     }
 }
